5d2490ba778eb26efc699951367befb823c42969
Giovanni d'Amico <giovanni.damico@edu.unito.it>
2019-07-29 15:04:51 +0200

    A simple Introduction to Inheritance Sealed class

---
+++ 01_BasicInheritance/01_BasicInheritance/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_BasicInheritance
+{
+    class Car
+    {
+        public readonly int MaxSpeed;
+        private int currSpeed;
+
+        public Car(int max)
+        {
+            MaxSpeed = max;
+        }
+        public Car()
+        {
+            MaxSpeed = 55;
+        }
+        public int Speed
+        {
+            get { return currSpeed; }
+            set
+            {
+                currSpeed = value;
+                if (currSpeed > MaxSpeed)
+                    currSpeed = MaxSpeed;
+            }
+        }
+
+    }
+}
---
+++ 01_BasicInheritance/01_BasicInheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Exploring Inheritance ****");
+            Car MyCar = new Car(80);
+
+            MyCar.Speed = 50;
+            Console.WriteLine("My car is going at {0} Kmh", MyCar.Speed);
+
+            MiniVan MyMini = new MiniVan();
+            MyMini.Speed = 10;
+            Console.WriteLine("MyMini is going at {0} Kmh", MyMini.Speed);
+        }
+    }
+}
